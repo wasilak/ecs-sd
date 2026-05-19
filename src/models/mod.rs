13960b5f04767cf7f01299c0e66b774330eaa5b9
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-05-19 18:09:26 +0200

    feat(phase-1-02): add FilterParams for query parameter filtering

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,2 +1,11 @@
 pub mod target;
 pub use target::Target;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct FilterParams {
+    pub cluster: Option<String>,
+    pub service: Option<String>,
+    pub family: Option<String>,
+}
